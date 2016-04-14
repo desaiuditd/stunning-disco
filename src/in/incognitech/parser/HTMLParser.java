b2961313a493af278a71df11f92a7cffca347a41
Udit Desai <desaiuditd@gmail.com>
2016-04-13 18:39:09 -0700

    adds eclipse structure

---
+++ src/in/incognitech/parser/HTMLParser.java
@@ -0,0 +1,25 @@
+package in.incognitech.parser;
+
+import java.net.URI;
+import java.util.List;
+
+public class HTMLParser {
+
+    public List<URI> retriveLinks() {
+        return null;
+    }
+
+    public String retriveContent() {
+        return null;
+    }
+
+    public void readHTML() {
+
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
