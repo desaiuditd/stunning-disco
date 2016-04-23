008883eef58b1466bd71d5d273e65e51846fdfed
Udit Desai <desaiuditd@gmail.com>
2016-04-22 17:40:53 -0700

    adds Cleaner class

---
+++ src/in/incognitech/cleaner/Cleaner.java
@@ -0,0 +1,13 @@
+package in.incognitech.cleaner;
+
+public class Cleaner {
+
+	public Cleaner() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void removeNoise(String htmlMarkup) {
+		
+	}
+
+}
